c89f68cf8eb1b72421c461fbda1251e0033e4ea0
root <root@DESKTOP-4M19GID.localdomain>
2022-03-10 22:50:10 +0800

    2203102250

---
+++ DataStructure/ST.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct ST{
+	vector<vector<int>>st;
+	ST(vector<int>A):st(A.size()){
+		int n=A.size()-1;
+		for(int i=1;i<=n;++i){
+			st[i].resize(__lg(n)+1);
+			st[i][0]=A[i];
+		}
+		for(int j=1;j<=__lg(n);++j)
+			for(int i=1;i+(1<<j)-1<=n;++i)
+				st[i][j]=min(st[i][j-1],st[i+(1<<(j-1))][j-1]);
+	}
+	int qry(int l,int r){
+		if(l>r)swap(l,r);
+		l++;
+		int s=__lg(r-l+1);
+		return min(st[l][s],st[r-(1<<s)+1][s]);
+	}
+};
+
+int main(){
+    
+    return 0; 
+}
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # xiongjx_template
 xiongjx's template for ACM-ICPC
+
+
+## 一些说明
+
+- 一般无特殊说明，数组、字符串等的下标均从 1 开始
