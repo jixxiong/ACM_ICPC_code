d40da7207d5de4ac5943bd72b9f8fd415e02b86a
root <root@DESKTOP-4M19GID.localdomain>
2022-03-11 19:37:23 +0800

    2022-03-11-19-37-23

--- DataStructure/01Trie.cpp
+++ DataStructure/01Trie.cpp
@@ -6,19 +6,19 @@ void fo(fst F, lst... L) { cerr<<F<<" "; fo(L...); }
 #define all(x) x.begin(),x.end()
 using ll=long long;
 
-template<int int_len=30>
+
 struct Trie{
 	using Node=array<int,2>;
 	vector<char> end; // 代表该节点是否为一个串的结尾节点
 	vector<Node> nxt;
 	int tot;
-	Trie():cnt(1),nxt(1),tot(0){ }
+	Trie():end(1),nxt(1),tot(0){ }
 	void insert(int x){
 		int p=0;
-		for(int i=int_len;i>=0;--i){
+		for(int i=2;i>=0;--i){
 			auto ch=(x>>i)&1;
 			if(!nxt[p][ch]){
-				cnt.emplace_back(0);
+				end.emplace_back(0);
 				nxt.emplace_back(Node{});
 				nxt[p][ch]=++tot;
 			}
@@ -28,7 +28,7 @@ struct Trie{
 	}
 	bool query(int x){
 		int p=0;
-		for(int i=int_len;i>=0;--i){
+		for(int i=2;i>=0;--i){
 			auto ch=(x>>i)&1;
 			if(!nxt[p][ch])
 				return false;
