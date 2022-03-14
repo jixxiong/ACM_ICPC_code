37aad5335537286b3ce03ec0984f30f1db2c8877
xiongjx751 <xiongjx751@qq.com>
2022-03-14 14:24:05 +0800

    2022-03-14-14-23-44

--- StringAlgorithms/SA.cpp
+++ StringAlgorithms/SA.cpp
@@ -1,72 +1,102 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define int long long
-#define all(x) (x).begin(),(x).end()
-
-template<size_t size>
-struct SuffixArray {
-    bool type[size << 1];
-    int bucket[size], bucket1[size];
-    int sa[size], rk[size], ht[size];
-    inline bool isLMS(const int i, const bool *type) { return i > 0 && type[i] && !type[i - 1]; }
-    template<class T>
-
-    inline void inducedSort(T s, int *sa, const int len, const int sigma, const int bucketSize, bool *type, int *bucket, int *cntbuf, int *p) {
-        memset(bucket, 0, sizeof(int) * sigma);
-        memset(sa, -1, sizeof(int) * len);
-        for (register int i = 0; i < len; i++) bucket[s[i]]++;
-        cntbuf[0] = bucket[0];
-        for (register int i = 1; i < sigma; i++) cntbuf[i] = cntbuf[i - 1] + bucket[i];
-        for (register int i = bucketSize - 1; i >= 0; i--) sa[--cntbuf[s[p[i]]]] = p[i];
-        for (register int i = 1; i < sigma; i++) cntbuf[i] = cntbuf[i - 1] + bucket[i - 1];
-        for (register int i = 0; i < len; i++) if (sa[i] > 0 && !type[sa[i] - 1]) sa[cntbuf[s[sa[i] - 1]]++] = sa[i] - 1;
-        cntbuf[0] = bucket[0];
-        for (register int i = 1; i < sigma; i++) cntbuf[i] = cntbuf[i - 1] + bucket[i];
-        for (register int i = len - 1; i >= 0; i--) if (sa[i] > 0 && type[sa[i] - 1]) sa[--cntbuf[s[sa[i] - 1]]] = sa[i] - 1;
-    }
-    template<typename T>
-    inline void sais(T s, int *sa, int len, bool *type, int *bucket, int *bucket1, int sigma) {
-        int i, j, bucketSize = 0, cnt = 0, p = -1, x, *cntbuf = bucket + sigma;
-        type[len - 1] = 1;
-        for (i = len - 2; i >= 0; i--) type[i] = s[i] < s[i + 1] || (s[i] == s[i + 1] && type[i + 1]);
-        for (i = 1; i < len; i++) if (type[i] && !type[i - 1]) bucket1[bucketSize++] = i;
-        inducedSort(s, sa, len, sigma, bucketSize, type, bucket, cntbuf, bucket1);
-        for (i = bucketSize = 0; i < len; i++) if (isLMS(sa[i], type)) sa[bucketSize++] = sa[i];
-        for (i = bucketSize; i < len; i++) sa[i] = -1;
-        for (i = 0; i < bucketSize; i++) {
-            x = sa[i];
-            for (j = 0; j < len; j++) {
-                if (p == -1 || s[x + j] != s[p + j] || type[x + j] != type[p + j]) { cnt++, p = x; break; }
-                else if (j > 0 && (isLMS(x + j, type) || isLMS(p + j, type))) break;
-            }
-            x = (~x & 1 ? x >> 1 : x - 1 >> 1), sa[bucketSize + x] = cnt - 1;
-        }
-        for (i = j = len - 1; i >= bucketSize; i--) if (sa[i] >= 0) sa[j--] = sa[i];
-        int *s1 = sa + len - bucketSize, *bucket2 = bucket1 + bucketSize;
-        if (cnt < bucketSize) sais(s1, sa, bucketSize, type + len, bucket, bucket1 + bucketSize, cnt);
-        else for (i = 0; i < bucketSize; i++) sa[s1[i]] = i;
-        for (i = 0; i < bucketSize; i++) bucket2[i] = bucket1[sa[i]];
-        inducedSort(s, sa, len, sigma, bucketSize, type, bucket, cntbuf, bucket2);
-    }
-    inline void getHeight(const char *s, const int len, const int *sa) {
-        for (int i = 0, k = 0; i < len; i++) {
-            if (rk[i] == 0) k = 0;
-            else {
-                if (k > 0) k--;
-                int j = sa[rk[i] - 1];
-                while (i + k < len && j + k < len && s[i + k] == s[j + k]) k++;
-            }
-            ht[rk[i]] = k;
-        }
-    }
-    template<class T>
-    inline void init(T s, const int len, const int sigma) {
-        sais(s, sa, len, type, bucket, bucket1, sigma);
-        for (register int i = 1; i < len; i++) rk[sa[i]] = i;
-        getHeight(s, len, sa);
-    }
+template<typename T,int sigma=300>
+struct SA{
+	vector<int>sa,rk,ht;
+	// Ensure that str[n] is the unique lexicographically smallest character in str.
+	// Moreover, the max element should not be larger than the size of the array
+	// this means you should discretize the array before
+	SA(T* str,int n){
+		const int size=max(sigma,n)+2;
+		sa.resize(size),rk.resize(size),ht.resize(size);
+		vector<int>s(size<<1),t(size<<1),p(size),cur(size),cnt(size);
+		str[++n]=0,++str;
+		int m=[&](){
+			int m=*max_element(str,str+n);
+			fill_n(&rk[0],m+1,0);
+			for (int i=0;i<n;i++) rk[(int)str[i]]=1;
+			for (int i=0;i<m;i++) rk[i+1]+=rk[i];
+			for (int i=0;i<n;i++) s[i]=rk[(int)str[i]]-1;
+			return rk[m];
+		}();
+		function<void(int,int,int*,int*,int*)>sais=[&](int n,int m,int* s,int* t,int* p) {
+			int n1=t[n-1]=0,ch=rk[0]=-1,*s1=s+n;
+			auto pushS=[&](int x){ sa[cur[s[x]]--]=x; };
+			auto pushL=[&](int x){ sa[cur[s[x]]++]=x; };
+			auto IS=[&](int* v){
+				fill_n(&sa[0],n,-1);fill_n(&cnt[0],m,0);
+				for(int i=0;i<n;i++) cnt[s[i]]++;
+				for(int i=1;i<m;i++) cnt[i]+=cnt[i-1];
+				for(int i=0;i<m;i++) cur[i]=cnt[i]-1;
+				for(int i=n1-1;~i;i--) pushS(v[i]);
+				for(int i=1;i<m;i++) cur[i]=cnt[i-1];
+				for(int i=0;i<n;i++) if(sa[i]>0&&t[sa[i]-1]) pushL(sa[i]-1);
+				for(int i=0;i<m;i++) cur[i]=cnt[i]-1;
+				for(int i=n-1;~i;i--) if(sa[i]>0&&!t[sa[i]-1]) pushS(sa[i]-1);
+			};
+			for(int i=n-2;~i;i--) t[i]=s[i]==s[i+1]?t[i+1]:s[i]>s[i+1];
+			for(int i=1;i<n;i++) rk[i]=t[i-1]&&!t[i]?(p[n1]=i,n1++):-1;
+			IS(p);
+			for(int i=0,x,y;i<n;i++) if(~(x=rk[sa[i]])){
+				if(ch<1||p[x+1]-p[x]!=p[y+1]-p[y])ch++;
+				else for(int j=p[x],k=p[y];j<=p[x+1];j++,k++)
+					if((s[j]<<1|t[j])!=(s[k]<<1|t[k])) { ch++; break; }
+				s1[y=x]=ch;
+			}
+			if(ch+1<n1)sais(n1,ch+1,s1,t+n,p+n1);
+			else for(int i=0;i<n1;i++) sa[s1[i]]=i;
+			for(int i=0;i<n1;i++) s1[i]=p[sa[i]];
+			IS(s1);
+		};
+		sais(n,m,&s[0],&t[0],&p[0]);
+		for (int i=0;i<n;i++) rk[sa[i]]=i;
+		for (int i=0,h=ht[0]=0;i<n-1;i++) {
+			int j=sa[rk[i]-1];
+			while (i+h<n&&j+h<n&&s[i+h]==s[j+h]) h++;
+			if ((ht[rk[i]]=h)) h--;
+		}
+		for(int i=n-1;i>=1;--i) {
+			rk[i]=rk[i-1];
+			sa[i]++;
+		}
+	}
 };
+
+char s[1000010];
+
+// ST for SA and static lcp => lcp(i,j) in O(1)
+struct ST{
+	vector<vector<int>>st;
+	ST(vector<int>A):st(A.size()){ // height 
+		int n=A.size()-1;
+		for(int i=1;i<=n;++i){
+			st[i].resize(__lg(n)+1);
+			st[i][0]=A[i];
+		}
+		for(int j=1;j<=__lg(n);++j)
+			for(int i=1;i+(1<<j)-1<=n;++i)
+				st[i][j]=min(st[i][j-1],st[i+(1<<(j-1))][j-1]);
+	}
+	int qry(int l,int r){ // lcp(l,r)
+		if(l==r) return -1;
+		if(l>r)swap(l,r);
+		l++;
+		int s=__lg(r-l+1);
+		return min(st[l][s],st[r-(1<<s)+1][s]);
+	}
+};
+
+
+int main() {
+	scanf("%s", s+1);
+	int n=strlen(s+1);
+	SA sa(s,n);
+	for (int i=1;i<=n;++i)
+		printf("%d ",sa.sa[i]);
+	puts("");
+	return 0;
+}
 
 // O(nlogn) => SA 
 struct SA {
@@ -102,27 +132,6 @@ struct SA {
 };
 
 
-// ST for SA and static lcp => lcp(i,j) in O(1)
-struct ST{
-	vector<vector<int>>st;
-	ST(vector<int>A):st(A.size()){ // height 
-		int n=A.size()-1;
-		for(int i=1;i<=n;++i){
-			st[i].resize(__lg(n)+1);
-			st[i][0]=A[i];
-		}
-		for(int j=1;j<=__lg(n);++j)
-			for(int i=1;i+(1<<j)-1<=n;++i)
-				st[i][j]=min(st[i][j-1],st[i+(1<<(j-1))][j-1]);
-	}
-	int qry(int l,int r){ // lcp(l,r)
-		if(l==r) return -1;
-		if(l>r)swap(l,r);
-		l++;
-		int s=__lg(r-l+1);
-		return min(st[l][s],st[r-(1<<s)+1][s]);
-	}
-};
 
 signed main(){
 	
