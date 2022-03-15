78e1eef11d9c5f05fa641cf110752304fb953fb7
root <root@DESKTOP-4M19GID.localdomain>
2022-03-15 22:37:25 +0800

    2022-03-15-22-37-25

--- StringAlgorithms/Manacher.cpp
+++ StringAlgorithms/Manacher.cpp
@@ -6,25 +6,35 @@ void fo(fst F, lst... L) { cerr<<F<<" "; fo(L...); }
 #define all(x) x.begin(),x.end()
 using ll=long long;
 
-struct Manacher{
-	int n; vector<int>p; string t; // p[i] => 以 i 为起点的最大回文半径
+struct Manacher:vector<int>{
+	// 经过处理后字符串变成 "$#1#2#3..."
+	vector<int>&p; // p[i] => 以 i 为起点的最大回文半径
 	int max_len; // 最大回文子串的长度
-	Manacher(const char* s,int len):n(2*len+1),p(n+1),t("$#"),max_len(0){
+	Manacher(const char* s,int len) 
+	  :vector<int>(2*len+2),p(*this),max_len(0){
+		int n=2*len+1;
+		string t="$#";
 		for(int i=1;i<=len;++i)
 			t+=s[i]+"#"s;
 		for(int k=1,r=0,mid=0;k<=n;++k){
 			if(k<=r) p[k]=min(p[(mid<<1)-k],r-k+1);
 			while(t[k-p[k]]==t[k+p[k]]) ++p[k];
 			if(p[k]+k>r) r=p[k]+k-1,mid=k;
-			if(p[k]>max_len) max_len=p[k];
+			if(p[k]-1>max_len) max_len=p[k]-1;
 		}
-		--max_len;
+		vector<int>ret(len+1);
+		for(int i=1;i<=len;++i)
+			ret[i]=p[i]-1;
+		vector<int>::operator=(ret);
 	}
 };
 
 int main(){
-	string str;
-	cin>>str;
-	printf("%d\n",Manacher(str.c_str()-1,str.length()).max_len);
+	int n;
+	scanf("%d",&n);
+	string str(n+2,0);
+	scanf("%s",&str[1]);
+	Manacher len(&str[0],n);
+	printf("%d\n",len.max_len);
 	return 0; 
 }
