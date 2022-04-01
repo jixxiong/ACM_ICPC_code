69f75636e2e1c8b44ee3df22d29f7a422bc82111
xiongjx751 <xiongjx751@qq.com>
2022-04-01 18:59:42 +0800

    2022-04-01-18-59-42

---
+++ pastebin/220401/nowcoder20220401/a.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+#ifndef LOCAL_DEBUG
+__attribute((constructor)) void before_main() { std::ios_base::sync_with_stdio(false);std::cin.tie(0);std::cout.tie(0); }
+#endif
+void fk(){ std::cerr<<'\n'; } template<class fst,class...lst>
+void fk(fst F, lst... L) { std::cerr<<F<<' '; fk(L...); }
+#define all(x) x.begin(),x.end()
+#define pb emplace_back
+using ll=long long;
+
+int main(){
+    
+    return 0; 
+}
