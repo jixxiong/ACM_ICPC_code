4f745abba2f7df18ba04849aabd05a2eb53454e1
xiongjx751 <xiongjx751@qq.com>
2022-04-23 18:24:38 +0800

    2022-04-23-18-24-38

--- pastebin/220422/cf1671/e.cpp
+++ pastebin/220422/cf1671/e.cpp
@@ -14,26 +14,15 @@ int main(){
     int n; std::cin>>n;
     n=(1<<n)-1;
     std::string s; std::cin>>s; s=' '+s;
-    const int mo=100000799;const int m2=1000000409;
     std::vector<int>id(n+1); int c=1;
-    std::vector<int>fk(n+1),fk2(n+1);
+    const int mo=998244353;
+    std::vector<std::string>hs(n+1);
     std::function<void(int)>f=[&](int u){
         id[u]=c++;
-        fk[u]=(s[u]=='A')?41:37;
-        fk2[u]=(s[u]=='A')?23:31;
+        hs[u]=std::string("")+s[u];
         if((u<<1)>n)return ;
-        f(u<<1); f(u<<1|1);
-        if((ll)fk[u<<1]*(ll)fk2[u<<1]<(ll)fk[u<<1|1]*(ll)fk2[u<<1|1]){
-            fk[u]=(ll)fk[u]*((3ll*fk[u<<1]%mo+233)%mo+fk[u<<1|1]%mo);
-            fk[u]%=mo;
-            fk2[u]=(ll)fk2[u]*((7ll*fk2[u<<1]%m2+4933)%m2+fk2[u<<1|1]%m2);
-            fk2[u]%=m2;
-        }else{
-            fk[u]=(ll)fk[u]*((3ll*fk[u<<1|1]%mo+233)%mo+fk[u<<1]%mo);
-            fk[u]%=mo;
-            fk2[u]=(ll)fk2[u]*((7ll*fk2[u<<1|1]%m2+4933)%m2+fk2[u<<1]%m2);
-            fk2[u]%=m2;
-        }
+        f(u<<1);f(u<<1|1);
+        hs[u]+=std::min(hs[u<<1],hs[u<<1|1])+std::max(hs[u<<1],hs[u<<1|1]);
     };
     f(1);
     std::function<int(int)>dfs=[&](int u){
@@ -41,7 +30,7 @@ int main(){
         if(v1>n) return 1ll;
         int m1=dfs(v1);
         int m2=dfs(v2);
-        return (ll)m1*m2%mo*(fk[v1]==fk[v2]&&fk2[v1]==fk2[v2]?1:2)%mo;
+        return (ll)m1*m2%mo*(hs[v1]==hs[v2]?1:2)%mo;
     };
     std::cout<<dfs(1)<<'\n';
     return 0;
