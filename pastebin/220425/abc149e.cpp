0fb2377b0e53200ffb782a4d94ead76c0b10acd4
xiongjx751 <xiongjx751@qq.com>
2022-04-26 20:27:04 +0800

    2022-04-26-20-27-04

--- pastebin/220425/abc149e.cpp
+++ pastebin/220425/abc149e.cpp
@@ -15,20 +15,6 @@ void add(int& x,int y){
     x+=y; if(x>=mo) x-=mo; if(x<0) x+=mo;
 }
 
-// struct Fenwick{
-//     int n;
-//     std::vector<int>arr;
-//     Fenwick(int n):n(n),arr(n+1){}
-//     void add(int x,int v){
-//         for(;x<=n;x+=x&-x) add(arr[x],v);
-//     }
-//     void get(int x){
-//         int ret=0;
-//         for(;x>0;x-=x&-x) add(ret,arr[x]);
-//         return ret;
-//     }
-// };
-
 int g(int x){
     if(x<10) return 2;
     if(x<100) return 3;
@@ -38,6 +24,10 @@ int g(int x){
 
 int main(){
     int n; std::cin>>n>>mo;
+    if(n==1) {
+        std::cout<<"0\n";
+        return 0;
+    }
     std::vector<std::vector<int>>f(n+1,std::vector<int>(n+1));
     for(int i=1;i<=n;++i){
         f[g(i)][i]=26;
@@ -45,17 +35,16 @@ int main(){
     int ret=0;
     for(int j=1;j<=n;++j){
         for(int i=1;i<=n;++i){
-            // if(g(i)==j) continue;
+            if(g(i)>=j) continue;
             if(j>3) add(f[j][i],25ll*(f[j-2][std::max(i-1,0)]   -f[j-2][std::max(i-10,0)]   +mo)%mo);
             if(j>4) add(f[j][i],25ll*(f[j-3][std::max(i-10,0)]  -f[j-3][std::max(i-100,0)]  +mo)%mo);
             if(j>5) add(f[j][i],25ll*(f[j-4][std::max(i-100,0)] -f[j-4][std::max(i-1000,0)] +mo)%mo);
             if(j>6) add(f[j][i],25ll*(f[j-5][std::max(i-1000,0)]-f[j-5][std::max(i-10000,0)]+mo)%mo);
-            // std::cout<<f[j][i]<<" \n"[i==n];
         }
-        add(ret,f[j][n]); 
+        if(j!=n) add(ret,f[j][n]);
+        // for(int i=1;i<=n;++i) std::cout<<f[j][i]<<" \n"[i==n];
         for(int i=1;i<=n;++i) add(f[j][i],f[j][i-1]);
     }
-    // for(int i=1;i<=n;++i) assert((f[g(i)][i]-f[g(i)][i-1]+mo)%mo==26);
     std::cout<<ret<<'\n';
     return 0;
 }
