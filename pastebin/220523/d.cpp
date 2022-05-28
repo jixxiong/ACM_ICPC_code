b4740f8a2b8138c67a1a1c02f76adc194a73ec5e
xiongjx751 <xiongjx751@qq.com>
2022-05-28 10:48:22 +0800

    2022-05-28-10-48-22

--- pastebin/220523/d.cpp
+++ pastebin/220523/d.cpp
@@ -1,12 +1,12 @@
 #include <bits/stdc++.h>
 
 #ifndef LOCAL_DEBUG
-__attribute((constructor)) void before_main() { std::ios_base::sync_with_stdio(false);std::cin.tie(0);std::cout.tie(0); }
-#define er(args...) void()
+    __attribute((constructor)) void before_main() { std::ios_base::sync_with_stdio(false);std::cin.tie(0);std::cout.tie(0); }
+    #define er(args...) void()
 #else
-#define vwn(x) #x"=",x,"\n"
-void _er(){ std::cerr<<'\n'; } template<class F,class...L> void _er(F f,L...l) { std::cerr<<f<<' ';  _er(l...); }
-#define er(args...) _er("\n---- line :",__LINE__,"----"); _er(args); _er("---- line :",__LINE__,"----\n")
+    #define vwn(x) #x"=",x,"\n"
+    void _er(){ std::cerr<<'\n'; } template<class F,class...L> void _er(F f,L...l) { std::cerr<<f<<' ';  _er(l...); }
+    #define er(args...) _er("\n---- line :",__LINE__,"----"); _er(args); _er("---- line :",__LINE__,"----\n")
 #endif
 
 #define all(x) x.begin(),x.end()
@@ -17,3 +17,4 @@ int32_t main(){
     
     return 0;
 }
+
