460ef65607d1415400681e7a3af3c019ee69cbc7
xiongjx751 <xiongjx751@qq.com>
2022-09-20 09:37:13 +0800

    20

--- README.md
+++ README.md
@@ -1,13 +1,12 @@
 # xiongjx_template
 xiongjx's template for ACM-ICPC
-
+<!-- 
 
 ## 一些说明
 
 本模板企图谋求 使用便利性、代码可读性、程序高效性 三者之间的平衡
 
 - 一般的数据结构都会以 struct 的形式封装方便调用（oop 与 高效并无矛盾）
-
 - 由于利用 struct 将命名空间区别开了，所以不必过多担心命名冲突的问题
 
 - 本模板尽量避免预先开辟数组，而是在栈空间创建 std::vector 代替，减少 magic number 的出现
@@ -20,3 +19,4 @@ xiongjx's template for ACM-ICPC
 
 - 代码中会较多地使用 STL 的内容，若在实践过程中发现常数过大，可以将 STL 的相关内容用更高效的方式实现（如将 std::vector 改成堆区的数组等）
 
+ -->
---
+++ pastebin/220919/a.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+
+#ifndef LOCAL_COMPILE
+__attribute((constructor))void unbind(){std::ios_base::sync_with_stdio(false);std::cin.tie(0);std::cout.tie(0);}
+#define du(...) void()
+#else
+void _du(){std::cerr<<'\n';}template<class F,class...L>void _du(F f,L...l){std::cerr<<f<<' ';_du(l...);}
+#define du(args...){std::cerr<<"[LINE "<<__LINE__<<"]: ";_du(args);}
+#endif
+
+#define all(x) x.begin(),x.end()
+#define pb emplace_back
+#define int long long
+#define ld long double
+#define ll long long
+#define i32 int32_t
+#define i64 int64_t
+#define i128 __int128
+template<typename T>using vc=std::vector<T>;
+template<typename T>using vvc=std::vector<vc<T>>;
+template<typename T>using vvvc=std::vector<vvc<T>>;
+template<typename T>using vvvvc=std::vector<vvvc<T>>;
+template<typename T>using vvvvvc=std::vector<vvvvc<T>>;
+template<typename T>vvc<T> vcc(i32 s1=0,i32 s2=0,const T&init=T()){ return vvc<T>(s1,vc<T>(s2,init)); }
+template<typename T>vvvc<T> vccc(i32 s1=0,i32 s2=0,i32 s3=0,const T&init=T()){ return vvvc<T>(s1,vcc(s2,s3,init)); }
+template<typename T>vvvvc<T> vcccc(i32 s1=0,i32 s2=0,i32 s3=0,i32 s4=0,const T&init=T()){ return vvvvc<T>(s1,vccc(s2,s3,s4,init)); }
+template<typename T>vvvvvc<T> vccccc(i32 s1=0,i32 s2=0,i32 s3=0,i32 s4=0,i32 s5=0,const T&init=T()){ return vvvvvc<T>(s1,vcccc(s2,s3,s4,s5,init)); }
+
+ld const EPS=1e-8;
+ld const PI=std::acos((ld)-1.0);
+ll const mod=998244353;
+
+int32_t main(){
+    int a,b; std::cin>>a>>b;
+    std::cout<<a+b<<'\n';
+    return 0;
+}
